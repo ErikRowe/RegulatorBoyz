7aaa77c2a143ade626470d035fea40acaeedd2d3
ErikRowe <erike.rowe@outlook.com>
2021-08-26 15:50:09 +0200

    gay

--- TK4225-Systemteori/functions.py
+++ TK4225-Systemteori/functions.py
@@ -1,4 +1,18 @@
-class sisBoyz(object):
+import numpy as np
+import math
+
+class cisBoyz(object):
     def __init__(self):
         super(sisBoyz, self).__init__()
+
+    def test(self,param1,param2):
+        """
+        ***\n 
+        This is a test
+        params:
+            - param1
+            - param2 
+        \n***
+        """
+           
         
