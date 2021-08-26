28bcc994eb9a5d3a6be45ed86abe36e11089cac6
ErikRowe <erike.rowe@outlook.com>
2021-08-26 16:20:32 +0200

    her kommer det

--- TK4225/functions.py
+++ TK4225/functions.py
@@ -1,5 +1,4 @@
-import numpy as np
-import math
+
 
 class cisBoyz(object):
     def __init__(self):
@@ -7,16 +6,36 @@ class cisBoyz(object):
 
     def example(self,param1,param2):
         """
-        *** Description *** 
+        *** Description ***\n
         This is a test
 
-        *** Function ***
+        *** Function ***\n
         params:
             - param1
             - param2
-
         returns:
             - string with params 
         """
         print("u are geh", param1, param2)
+
+    def LotkaVolterra(self, y, t):
+        #
+        # parameters
+        alpha = 1.1
+        beta  = 0.7
+        gamma = 0.4
+        delta = 0.1
+        #
+        # get the individual variables - for readability
+        yPrey = y[0]
+        yPred = y[1]
+        #
+        # individual derivatives
+        dyPreydt  =   alpha * yPrey - beta  * yPrey * yPred
+        dyPreddt  = - gamma * yPred + delta * yPrey * yPred
+        #
+        return [ dyPreydt, dyPreddt ]
+
+    
+        
         
--- innlevering1.py
+++ innlevering1.py
@@ -1,8 +1,44 @@
 import numpy as np
+import math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 from TK4225.functions import cisBoyz
 
 def main():
-    test = cisBoyz()
-    test.example(1,"supergay")
+    mainClass = cisBoyz()
+
+    # define the initial condition
+    y0 = [ 20, 5 ]
+
+    # define the time points where the solution is computed
+    n    = 100
+    tmax = 20
+    t    = np.linspace(0, tmax, n)
+
+    # solve the ODE
+    y = odeint(mainClass.LotkaVolterra, y0, t)
+
+    # get the individual variables
+    yPrey = y[:,0]
+    yPred = y[:,1]
+
+    # plot the time evolution
+    plt.subplot(1,2,1)
+    plt.plot(t, yPrey, label='prey', linewidth=1)
+    plt.plot(t, yPred, label='predator', linewidth=1)
+    plt.grid()
+    plt.legend(loc = 'upper right')
+    plt.xlabel('time evolution')
+
+    # plot the phase portrait
+    plt.subplot(1,2,2)
+    plt.plot(yPrey, yPred, linewidth=1)
+    plt.grid()
+    plt.xlabel('prey')
+    plt.ylabel('predator')
+    plt.title('phase portrait')
+
+    plt.show()
+    
 
 main()
