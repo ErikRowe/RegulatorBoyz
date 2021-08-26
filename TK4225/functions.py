ec9efba6755d86065fba7066740a3708e3cba2a2
ErikRowe <erike.rowe@outlook.com>
2021-08-26 16:00:51 +0200

    updates

---
+++ TK4225/functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+class cisBoyz(object):
+    def __init__(self):
+        super(cisBoyz, self).__init__()
+
+    def example(self,param1,param2):
+        """
+        *** Description *** 
+        This is a test
+
+        *** Function ***
+        params:
+            - param1
+            - param2
+
+        returns:
+            - string with params 
+        """
+        print("u are geh", param1, param2)
+        
---
+++ innlevering1.py
@@ -0,0 +1,8 @@
+import numpy as np
+from TK4225.functions import cisBoyz
+
+def main():
+    test = cisBoyz()
+    test.example(1,"supergay")
+
+main()
